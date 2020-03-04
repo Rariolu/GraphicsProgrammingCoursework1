3b8be0f9ae0caeda731c11e7f93cdb7eebb871ba
Rariolu <rcoyneyr82011@gmail.com>
2020-03-04 18:02:05 +0000

    SDLWindowManager added

---
+++ GraphicsProgrammingCoursework1/SDLWindowManager.cpp
@@ -0,0 +1,104 @@
+#include "SDLWindowManager.h"
+
+SDLWindowManager* SDLWindowManager::instance = nullptr;
+
+SDLWindowManager::SDLWindowManager()
+{
+	screenHeight = screenWidth = 0;
+	openGLContext = 0;
+	sdlWindow = 0;
+}
+
+SDLWindowManager::~SDLWindowManager()
+{
+	SDL_GL_DeleteContext(openGLContext); // delete context
+	SDL_DestroyWindow(sdlWindow); // detete window (make sure to delete the context and the window in the opposite order of creation in initDisplay())
+	SDL_Quit();
+}
+
+bool SDLWindowManager::GenerateWindowManager(string windowTitle, float width, float height, SDLWindowManager* sdlWindowManager)
+{
+	sdlWindowManager = new SDLWindowManager();
+	instance = sdlWindowManager;
+	return sdlWindowManager->Initialise(windowTitle, width, height);
+}
+
+SDLWindowManager* SDLWindowManager::Instance()
+{
+	return instance;
+}
+
+void SDLWindowManager::ClearDisplay(float r, float g, float b, float a)
+{
+	//Clear the display so that only the given RGBA colour is displayed.
+	glClearColor(r, g, b, a);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+float SDLWindowManager::GetScreenHeight()
+{
+	return screenHeight;
+}
+
+float SDLWindowManager::GetScreenWidth()
+{
+	return screenWidth;
+}
+
+void SDLWindowManager::SwapBuffer()
+{
+	SDL_GL_SwapWindow(sdlWindow);
+}
+
+bool SDLWindowManager::Initialise(string windowTitle, float width, float height)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	screenHeight = height;
+	screenWidth = width;
+
+	const int bufferSize = 8;
+
+	//Set RGB buffer sizes
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, bufferSize);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, bufferSize);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, bufferSize);
+
+	//Create a double buffer (two RGB buffers that are alternated between).
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	//Attempt to create SDL window.
+	sdlWindow = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, (int)width, (int)height, SDL_WINDOW_OPENGL);
+	if (!sdlWindow)
+	{
+		Log("SDL Window was not created.");
+		return false;
+	}
+
+	//Attempt to create openGL context
+	openGLContext = new SDL_GLContext(SDL_GL_CreateContext(sdlWindow));
+	if (!(*openGLContext))
+	{
+		Log("OpenGL context was not created.");
+		return false;
+	}
+
+	//Attempt to initialise openGL
+	GLenum glState = glewInit();
+	if (glState != GLEW_OK)
+	{
+		Log("OpenGL failed to initialise.");
+		return false;
+	}
+
+	//Clear the display to show only the given RGBA colour.
+	glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+
+	glEnable(GL_DEPTH_TEST);
+
+	//Tell openGL to only render polygons that should
+	//be visible from the camera's perspective.
+	glEnable(GL_CULL_FACE);
+
+	return true;
+}
---
+++ GraphicsProgrammingCoursework1/SDLWindowManager.h
@@ -0,0 +1,65 @@
+#ifndef _SDLWINDOWMANAGER_H
+#define _SDLWINDOWMANAGER_H
+
+#include "SDL.h"
+#include "glew.h"
+#include "Logger.h"
+
+class SDLWindowManager
+{
+	public:
+		//Destroy this instance of "SDLWindowManager",
+		//deleting the openGL context and the sdl window
+		//in the process.
+		~SDLWindowManager();
+
+		//Attempt to create and initialse an SDL window and return true if
+		//it succeeds (and return false if it fails). Output the created 
+		//instance of "SDLWindowManager".
+		static bool GenerateWindowManager(string windowTitle, float width, float height, SDLWindowManager* sdlwindowmanager);
+
+		//Return a pointer to an instance of "SDLWindowManager"
+		//(or create one if one hasn't been created yet).
+		static SDLWindowManager* Instance();
+
+		//Clear the window so that a colour described by the 
+		//RGBA values is the only one onscreen.
+		void ClearDisplay(float r, float g, float b, float a);
+
+		//Return the height of the window.
+		float GetScreenHeight();
+
+		//Return the width of the window.
+		float GetScreenWidth();
+
+		//Swap from the front window buffer to the back
+		//window buffer (which becomes the new front buffer).
+		void SwapBuffer();
+
+	private:
+		//Create a new instance of "SDLWindowManager"
+		//(private so that instances can only be created by the
+		//"Instance" method).
+		SDLWindowManager();
+
+		//Attempt to create and initialse an SDL window and return true if
+		//it succeeds (and return false if it fails).
+		bool Initialise(string windowTitle, float width, float height);
+
+		//A pointer to the current instance of "SDLWindowManager".
+		static SDLWindowManager* instance;
+		int i = 4;
+		//The height of the window.
+		float screenHeight;
+
+		//The width of the window.
+		float screenWidth;
+
+		//A pointer to the openGL context.
+		SDL_GLContext* openGLContext;
+
+		//A pointer to the "SDL_Window" instance.
+		SDL_Window* sdlWindow;
+};
+
+#endif
