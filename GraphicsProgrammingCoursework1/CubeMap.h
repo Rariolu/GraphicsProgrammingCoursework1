789dcf632d7239eef0b2ca5465735b2f4a0a6c11
Rariolu <rcoyneyr82011@gmail.com>
2020-03-15 01:55:22 +0000

    Cubemap class added.

---
+++ GraphicsProgrammingCoursework1/CubeMap.h
@@ -0,0 +1,23 @@
+#ifndef _CUBEMAP_H
+#define _CUBEMAP_H
+
+#include "glew.h"
+#include "Aliases.h"
+#include "stb_image.h"
+#include "Logger.h"
+
+class CubeMap
+{
+	private:
+		const static unsigned int faceNum = 6;
+	public:
+		CubeMap(string facePath,string extension);
+		CubeMap(string facePaths[faceNum]);
+		void Bind();
+	private:
+		string* GetFacePaths(string facePath, string extension);
+		GLuint textureHandler;
+		
+};
+
+#endif
