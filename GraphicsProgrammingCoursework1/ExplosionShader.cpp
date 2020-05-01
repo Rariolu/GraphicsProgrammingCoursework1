fec28b163554de1fa1f9330be639b025591e0958
Rariolu <rcoyneyr82011@gmail.com>
2020-05-01 23:03:39 +0100

    Calling "Init" within constructors.

--- GraphicsProgrammingCoursework1/ExplosionShader.cpp
+++ GraphicsProgrammingCoursework1/ExplosionShader.cpp
@@ -2,12 +2,12 @@
 
 ExplosionShader::ExplosionShader(string vertFile, string fragFile) : QuadMShader(vertFile,fragFile)
 {
-
+	Init();
 }
 
 ExplosionShader::ExplosionShader(string geomFile, string vertFile, string fragFile) : QuadMShader(geomFile, vertFile, fragFile)
 {
-
+	Init();
 }
 
 void ExplosionShader::Init()
