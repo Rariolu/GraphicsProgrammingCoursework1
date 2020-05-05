084b5e9ef8e71180804e5090ba8809d44141939c
Rariolu <rcoyneyr82011@gmail.com>
2020-05-05 19:29:10 +0100

    Gameobject vector replaced with a dictionary

--- GraphicsProgrammingCoursework1/Scene.cpp
+++ GraphicsProgrammingCoursework1/Scene.cpp
@@ -1,203 +1,205 @@
 #include "Scene.h"
 
-Scene::Scene()
+namespace GraphicsProgramming
 {
-	audioDevice = AudioDevice::Instance();
-	sdlWindowManager = SDLWindowManager::Instance();
-	camera = new Camera(70.0f, sdlWindowManager->GetScreenWidth() / sdlWindowManager->GetScreenHeight(), 0.01f, 1000.0f);
-	resourceManager = ResourceManager::Instance();
-}
 
-Scene::~Scene()
-{
-	//Delete pointers in order to free up
-	//memory.
-	delete audioDevice;
-	delete camera;
-	Dispose();
-}
-
-void Scene::Dispose()
-{
-	initialised = false;
-	//Delete all the gameobjects from memory 
-	//to free up resources.
-	for (unsigned int i = 0; i < gameObjects.size(); i++)
+	Scene::Scene()
 	{
-		delete gameObjects[i];
+		audioDevice = AudioDevice::Instance();
+		sdlWindowManager = SDLWindowManager::Instance();
+		camera = new Camera(70.0f, sdlWindowManager->GetScreenWidth() / sdlWindowManager->GetScreenHeight(), 0.01f, 1000.0f);
+		resourceManager = ResourceManager::Instance();
 	}
-	gameObjects.clear();
-	nextScene = "";
-}
 
-void Scene::Initialise()
-{
-	initialised = true;
-	audioDevice->SetListener(camera);
-}
-
-string Scene::Run()
-{
-	//Only run game loop if the scene
-	//has been initialised.
-	if (initialised)
+	Scene::~Scene()
 	{
-		bool loop = true;
-		//While another scene hasn't been
-		//requested and the user's input
-		//and gameplay don't request the
-		//scene to end.
-		while (loop && nextScene == "")
+		//Delete pointers in order to free up
+		//memory.
+		delete audioDevice;
+		delete camera;
+		Dispose();
+	}
+
+	void Scene::Dispose()
+	{
+		initialised = false;
+		for (pair<int, GameObject*> goPair : gameObjects)
 		{
-			//Update the value of "deltaTime" to represent
-			//the amount of time since the last frame.
-			GenerateDeltaTime();
+			delete goPair.second;
+		}
+		//Delete all the gameobjects from memory 
+		//to free up resources.
+		gameObjects.clear();
+		nextScene = "";
+	}
 
-			//Process key and mouse events.
-			bool input = GetInput();
+	void Scene::Initialise()
+	{
+		initialised = true;
+		audioDevice->SetListener(camera);
+	}
 
-			//Process game logic.
-			bool update = Update();
+	string Scene::Run()
+	{
+		//Only run game loop if the scene
+		//has been initialised.
+		if (initialised)
+		{
+			bool loop = true;
+			//While another scene hasn't been
+			//requested and the user's input
+			//and gameplay don't request the
+			//scene to end.
+			while (loop && nextScene == "")
+			{
+				//Update the value of "deltaTime" to represent
+				//the amount of time since the last frame.
+				GenerateDeltaTime();
 
-			//Render the gameobjects and skybox.
-			Render();
-			loop = input && update;
+				//Process key and mouse events.
+				bool input = GetInput();
+
+				//Process game logic.
+				bool update = Update();
+
+				//Render the gameobjects and skybox.
+				Render();
+				loop = input && update;
+			}
+		}
+		return nextScene;
+	}
+
+	GameObject* Scene::AddGameObject(string meshName, string textureName, string shaderName)
+	{
+		//Get the mesh, texture, and shader from the resource manager using the given names.
+		return AddGameObject(resourceManager->GetMesh(meshName), resourceManager->GetModelTexture(textureName), (GameObjectShader*)resourceManager->GetShader(shaderName));
+	}
+
+	GameObject* Scene::AddGameObject(Mesh* mesh, ModelTexture* texture, GameObjectShader* shader)
+	{
+		//Create a new gameobject using the given mesh, shader, and texture.
+		GameObject* gameObject = new GameObject(mesh, shader, texture);
+
+		//Add the gameobject to the collection of gameobjects to be rendered.
+		AddGameObject(gameObject);
+
+		//Return the gameobject pointer so it can be used elsewhere.
+		return gameObject;
+	}
+
+	void Scene::AddGameObject(GameObject* gameObject)
+	{
+		//Set the camera that's viewing the gameobject
+		//to be the one from this scene so it is rendered
+		//according to the position and orientation of that
+		//camera.
+		gameObject->SetCamera(camera);
+		//gameObjects.push_back(gameObject);
+		gameObjects.insert(std::make_pair((int)gameObject, gameObject));
+
+	}
+
+	float Scene::DeltaTime()
+	{
+		return deltaTime;
+	}
+
+	void Scene::GenerateDeltaTime()
+	{
+		//Determine the current time stamp and subtract
+		//the previous time stamp to determine
+		//the amount of time that occurred between frames.
+		float currentTime = (float)SDL_GetTicks() / 1000.0f;
+		float milliseconds = (currentTime - previousTimeStamp);
+		previousTimeStamp = currentTime;
+		deltaTime = milliseconds;
+	}
+
+	void Scene::RemoveGameObject(GameObject* gameObject)
+	{
+		if (gameObjects.size() > 0)
+		{
+			map<int, GameObject*>::iterator iter = gameObjects.find((int)gameObject);
+			if (iter != gameObjects.end())
+			{
+				gameObjects.erase(iter);
+			}
 		}
 	}
-	return nextScene;
-}
 
-GameObject* Scene::AddGameObject(string meshName, string textureName, string shaderName)
-{
-	//Get the mesh, texture, and shader from the resource manager using the given names.
-	return AddGameObject(resourceManager->GetMesh(meshName), resourceManager->GetModelTexture(textureName), (GameObjectShader*)resourceManager->GetShader(shaderName));
-}
-
-GameObject* Scene::AddGameObject(Mesh* mesh, ModelTexture* texture, GameObjectShader* shader)
-{
-	//Create a new gameobject using the given mesh, shader, and texture.
-	GameObject* gameObject = new GameObject(mesh, shader, texture);
-
-	//Add the gameobject to the collection of gameobjects to be rendered.
-	AddGameObject(gameObject);
-
-	//Return the gameobject pointer so it can be used elsewhere.
-	return gameObject;
-}
-
-void Scene::AddGameObject(GameObject* gameObject)
-{
-	//Set the camera that's viewing the gameobject
-	//to be the one from this scene so it is rendered
-	//according to the position and orientation of that
-	//camera.
-	gameObject->SetCamera(camera);
-	gameObjects.push_back(gameObject);
-}
-
-float Scene::DeltaTime()
-{
-	return deltaTime;
-}
-
-void Scene::GenerateDeltaTime()
-{
-	//Determine the current time stamp and subtract
-	//the previous time stamp to determine
-	//the amount of time that occurred between frames.
-	float currentTime = (float)SDL_GetTicks() / 1000.0f;
-	float milliseconds = (currentTime - previousTimeStamp);
-	previousTimeStamp = currentTime;
-	deltaTime = milliseconds;
-}
-
-void Scene::RemoveGameObject(GameObject* gameObject)
-{
-	//Iterate through the collection of gameobjects to find
-	//the one that's requested, then erase it from the collection.
-	if (gameObjects.size() > 0)
+	void Scene::SetNextScene(string scenename)
 	{
-		for (vector<GameObject*>::iterator i = gameObjects.begin(); i < gameObjects.end(); i++)
+		nextScene = scenename;
+	}
+
+	void Scene::SetSkyBox(SkyBox* _skybox)
+	{
+		skybox = _skybox;
+		skybox->SetCamera(camera);
+	}
+
+	bool Scene::GetInput()
+	{
+		//Iterate through all SDL events and process them.
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
 		{
-			if (gameObject == (*i))
+			switch (event.type)
 			{
-				gameObjects.erase(i);
+			case SDL_QUIT:
+			{
+				return false;
+			}
+			case SDL_KEYDOWN:
+			{
+				SDL_Keycode keycode = event.key.keysym.sym;
+				if (!KeyDown(keycode))
+				{
+					return false;
+				}
 				break;
 			}
-		}
-	}
-}
-
-void Scene::SetNextScene(string scenename)
-{
-	nextScene = scenename;
-}
-
-void Scene::SetSkyBox(SkyBox* _skybox)
-{
-	skybox = _skybox;
-	skybox->SetCamera(camera);
-}
-
-bool Scene::GetInput()
-{
-	//Iterate through all SDL events and process them.
-	SDL_Event event;
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-		case SDL_QUIT:
-		{
-			return false;
-		}
-		case SDL_KEYDOWN:
-		{
-			SDL_Keycode keycode = event.key.keysym.sym;
-			if (!KeyDown(keycode))
+			case SDL_MOUSEBUTTONDOWN:
 			{
-				return false;
+				SDL_MouseButtonEvent mousebutton = event.button;
+				if (!MouseDown(mousebutton))
+				{
+					return false;
+				}
+				break;
 			}
-			break;
-		}
-		case SDL_MOUSEBUTTONDOWN:
-		{
-			SDL_MouseButtonEvent mousebutton = event.button;
-			if (!MouseDown(mousebutton))
+			default:
 			{
-				return false;
+				break;
 			}
-			break;
+			}
 		}
-		default:
+		return true;
+	}
+
+	void Scene::Render()
+	{
+		//Clear the display so that it only shows the specified
+		//RGBA values.
+		sdlWindowManager->ClearDisplay(1.0f, 0.5f, 1.0f, 1.0f);
+
+		//Iterate through the gameobjects and render them.
+		for (pair<int,GameObject*> goPair : gameObjects)//(GameObject* go : gameObjects)
 		{
-			break;
+			goPair.second->Render();
 		}
+
+		if (skybox)
+		{
+			skybox->Render();
 		}
-	}
-	return true;
-}
 
-void Scene::Render()
-{
-	//Clear the display so that it only shows the specified
-	//RGBA values.
-	sdlWindowManager->ClearDisplay(1.0f, 0.5f, 1.0f, 1.0f);
+		glEnableClientState(GL_COLOR_ARRAY);
+		glEnd();
 
-	//Iterate through the gameobjects and render them.
-	for (GameObject* go : gameObjects)
-	{
-		go->Render();
+		//Swap the window buffers
+		sdlWindowManager->SwapBuffer();
 	}
 
-	if (skybox)
-	{
-		skybox->Render();
-	}
-
-	glEnableClientState(GL_COLOR_ARRAY);
-	glEnd();
-
-	//Swap the window buffers
-	sdlWindowManager->SwapBuffer();
 }
--- GraphicsProgrammingCoursework1/Scene.h
+++ GraphicsProgrammingCoursework1/Scene.h
@@ -8,113 +8,119 @@
 #include "AudioDevice.h"
 #include "Skybox.h"
 
-class Scene
+namespace GraphicsProgramming
 {
-public:
-	Scene();
-	~Scene();
 
-	//The method to be called when ending the scene and "disposing"
-	//its resources.
-	virtual void Dispose();
+	class Scene
+	{
+	public:
+		Scene();
+		~Scene();
 
-	//The method to be called to set up the scene whenever it's started
-	//or restarted.
-	virtual void Initialise();
+		//The method to be called when ending the scene and "disposing"
+		//its resources.
+		virtual void Dispose();
 
-	//Begin the game loop of this scene
-	//and iterate until the next scene
-	//is designated or a quit event is
-	//triggered.
-	string Run();
+		//The method to be called to set up the scene whenever it's started
+		//or restarted.
+		virtual void Initialise();
 
-protected:
-	//Create an instance of "GameObject" (using the name of a mesh, texture, and shader)
-	//and add it to the list of gameobjects which get renderered in the game loop.
-	GameObject* AddGameObject(string meshName, string textureName, string shaderName);
+		//Begin the game loop of this scene
+		//and iterate until the next scene
+		//is designated or a quit event is
+		//triggered.
+		string Run();
 
-	//Create an instance of "GameObject" (using a mesh, texture, and shader)
-	//and add it to the list of gameobjects which get renderered in the game loop.
-	GameObject* AddGameObject(Mesh* mesh, ModelTexture* texture, GameObjectShader* shader);
+	protected:
+		//Create an instance of "GameObject" (using the name of a mesh, texture, and shader)
+		//and add it to the list of gameobjects which get renderered in the game loop.
+		GameObject* AddGameObject(string meshName, string textureName, string shaderName);
 
-	//Add a given gameobject to the list of gameobjects which get
-	//renderered in the game loop.
-	void AddGameObject(GameObject* gameObject);
+		//Create an instance of "GameObject" (using a mesh, texture, and shader)
+		//and add it to the list of gameobjects which get renderered in the game loop.
+		GameObject* AddGameObject(Mesh* mesh, ModelTexture* texture, GameObjectShader* shader);
 
-	//Return the quantity of seconds that have passed between the previous frame and
-	//the current one.
-	float DeltaTime();
+		//Add a given gameobject to the list of gameobjects which get
+		//renderered in the game loop.
+		void AddGameObject(GameObject* gameObject);
 
-	//Calculate the quantity of seconds that have passed between the previous
-	//frame and the current one.
-	void GenerateDeltaTime();
+		//Return the quantity of seconds that have passed between the previous frame and
+		//the current one.
+		float DeltaTime();
 
-	//Remove the given gameobject from the collection
-	//of those that are rendered.
-	void RemoveGameObject(GameObject* gameObject);
+		//Calculate the quantity of seconds that have passed between the previous
+		//frame and the current one.
+		void GenerateDeltaTime();
 
-	//Designate the next scene to be loaded.
-	void SetNextScene(string scenename);
+		//Remove the given gameobject from the collection
+		//of those that are rendered.
+		void RemoveGameObject(GameObject* gameObject);
 
-	void SetSkyBox(SkyBox* _skybox);
+		//Designate the next scene to be loaded.
+		void SetNextScene(string scenename);
 
-	//A pointer to an instance of "AudioDevice" used to play
-	//sounds using the system's default audio device.
-	AudioDevice* audioDevice;
+		void SetSkyBox(SkyBox* _skybox);
 
-	//The camera that's used to render the gameobjects
-	//from a particular perspective.
-	Camera* camera;
+		//A pointer to an instance of "AudioDevice" used to play
+		//sounds using the system's default audio device.
+		AudioDevice* audioDevice;
 
-private:
-	//Process the user's input and return true if
-	//the input determines that the scene loop should
-	//continue (and return false if it shouldn't).
-	bool GetInput();
+		//The camera that's used to render the gameobjects
+		//from a particular perspective.
+		Camera* camera;
 
-	//Process a key down event and return true if
-	//the input determines that the scene loop should
-	//continue (and return false if it shouldn't).
-	virtual bool KeyDown(SDL_Keycode keycode) = 0;
+	private:
+		//Process the user's input and return true if
+		//the input determines that the scene loop should
+		//continue (and return false if it shouldn't).
+		bool GetInput();
 
-	//Process a mouse event and return true if
-	//the input determines that the scene loop should
-	//continue (and return false if it shouldn't).
-	virtual bool MouseDown(SDL_MouseButtonEvent mousebutton) = 0;
+		//Process a key down event and return true if
+		//the input determines that the scene loop should
+		//continue (and return false if it shouldn't).
+		virtual bool KeyDown(SDL_Keycode keycode) = 0;
 
-	//Iterate through all the gameobjects and render them.
-	void Render();
+		//Process a mouse event and return true if
+		//the input determines that the scene loop should
+		//continue (and return false if it shouldn't).
+		virtual bool MouseDown(SDL_MouseButtonEvent mousebutton) = 0;
 
-	//A vague method intended to be used to update the state
-	//of game objects and return true if the scene loop should
-	//continue (and return false if it shouldn't).
-	virtual bool Update() = 0;
+		//Iterate through all the gameobjects and render them.
+		void Render();
 
-	//The quantity of seconds that have passed between the previous frame and
-	//the current one.
-	float deltaTime = 0;
+		//A vague method intended to be used to update the state
+		//of game objects and return true if the scene loop should
+		//continue (and return false if it shouldn't).
+		virtual bool Update() = 0;
 
-	//A value which is true if the "Initialise" method has been called
-	//(and is false if it hasn't).
-	bool initialised = false;
+		//The quantity of seconds that have passed between the previous frame and
+		//the current one.
+		float deltaTime = 0;
 
-	//The list of gameobjects to be rendered.
-	vector<GameObject*> gameObjects;
+		//A value which is true if the "Initialise" method has been called
+		//(and is false if it hasn't).
+		bool initialised = false;
 
-	//The name of the next scene to be loaded.
-	string nextScene = "";
+		//The list of gameobjects to be rendered.
+		//vector<GameObject*> gameObjects;
+		map<int, GameObject*> gameObjects;
 
-	//The time stamp of the previous frame.
-	float previousTimeStamp = 0;
+		//The name of the next scene to be loaded.
+		string nextScene = "";
 
-	//A pointer to the resource manager.
-	ResourceManager* resourceManager;
+		//The time stamp of the previous frame.
+		float previousTimeStamp = 0;
 
-	//The sdl window manager of the current openGL
-	//context (used to clear the display and swap buffers).
-	SDLWindowManager* sdlWindowManager;
+		//A pointer to the resource manager.
+		ResourceManager* resourceManager;
 
-	SkyBox* skybox;
-};
+		//The sdl window manager of the current openGL
+		//context (used to clear the display and swap buffers).
+		SDLWindowManager* sdlWindowManager;
+
+		SkyBox* skybox;
+	};
+
+}
 
 #endif
