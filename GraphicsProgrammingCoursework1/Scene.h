74d807b0ba13a85e0d9cb210ee3749d669d70fd6
Rariolu <rcoyneyr82011@gmail.com>
2020-05-06 00:05:42 +0100

    Moving "GenerateDeltaTime" to private

--- GraphicsProgrammingCoursework1/Scene.h
+++ GraphicsProgrammingCoursework1/Scene.h
@@ -13,112 +13,115 @@ namespace GraphicsProgramming
 
 	class Scene
 	{
-	public:
-		Scene();
-		~Scene();
+		public:
+			Scene();
+			~Scene();
 
-		//The method to be called when ending the scene and "disposing"
-		//its resources.
-		virtual void Dispose();
+			//The method to be called when ending the scene and "disposing"
+			//its resources.
+			virtual void Dispose();
 
-		//The method to be called to set up the scene whenever it's started
-		//or restarted.
-		virtual void Initialise();
+			//The method to be called to set up the scene whenever it's started
+			//or restarted.
+			virtual void Initialise();
 
-		//Begin the game loop of this scene
-		//and iterate until the next scene
-		//is designated or a quit event is
-		//triggered.
-		string Run();
+			//Begin the game loop of this scene
+			//and iterate until the next scene
+			//is designated or a quit event is
+			//triggered.
+			string Run();
 
-	protected:
-		//Create an instance of "GameObject" (using the name of a mesh, texture, and shader)
-		//and add it to the list of gameobjects which get renderered in the game loop.
-		GameObject* AddGameObject(string meshName, string textureName, string shaderName);
+		protected:
+			//Create an instance of "GameObject" (using the name of a mesh, texture, and shader)
+			//and add it to the list of gameobjects which get renderered in the game loop.
+			GameObject* AddGameObject(string meshName, string textureName, string shaderName);
 
-		//Create an instance of "GameObject" (using a mesh, texture, and shader)
-		//and add it to the list of gameobjects which get renderered in the game loop.
-		GameObject* AddGameObject(Mesh* mesh, ModelTexture* texture, GameObjectShader* shader);
+			//Create an instance of "GameObject" (using a mesh, texture, and shader)
+			//and add it to the list of gameobjects which get renderered in the game loop.
+			GameObject* AddGameObject(Mesh* mesh, ModelTexture* texture, GameObjectShader* shader);
 
-		//Add a given gameobject to the list of gameobjects which get
-		//renderered in the game loop.
-		void AddGameObject(GameObject* gameObject);
+			//Add a given gameobject to the list of gameobjects which get
+			//renderered in the game loop.
+			void AddGameObject(GameObject* gameObject);
 
-		//Return the quantity of seconds that have passed between the previous frame and
-		//the current one.
-		float DeltaTime();
+			//Return the quantity of seconds that have passed between the previous frame and
+			//the current one.
+			float DeltaTime();
 
-		//Calculate the quantity of seconds that have passed between the previous
-		//frame and the current one.
-		void GenerateDeltaTime();
 
-		//Remove the given gameobject from the collection
-		//of those that are rendered.
-		void RemoveGameObject(GameObject* gameObject);
 
-		//Designate the next scene to be loaded.
-		void SetNextScene(string scenename);
+			//Remove the given gameobject from the collection
+			//of those that are rendered.
+			void RemoveGameObject(GameObject* gameObject);
 
-		void SetSkyBox(SkyBox* _skybox);
+			//Designate the next scene to be loaded.
+			void SetNextScene(string scenename);
 
-		//A pointer to an instance of "AudioDevice" used to play
-		//sounds using the system's default audio device.
-		AudioDevice* audioDevice;
+			void SetSkyBox(SkyBox* _skybox);
 
-		//The camera that's used to render the gameobjects
-		//from a particular perspective.
-		Camera* camera;
+			//A pointer to an instance of "AudioDevice" used to play
+			//sounds using the system's default audio device.
+			AudioDevice* audioDevice;
 
-	private:
-		//Process the user's input and return true if
-		//the input determines that the scene loop should
-		//continue (and return false if it shouldn't).
-		bool GetInput();
+			//The camera that's used to render the gameobjects
+			//from a particular perspective.
+			Camera* camera;
 
-		//Process a key down event and return true if
-		//the input determines that the scene loop should
-		//continue (and return false if it shouldn't).
-		virtual bool KeyDown(SDL_Keycode keycode) = 0;
+		private:
 
-		//Process a mouse event and return true if
-		//the input determines that the scene loop should
-		//continue (and return false if it shouldn't).
-		virtual bool MouseDown(SDL_MouseButtonEvent mousebutton) = 0;
+			//Calculate the quantity of seconds that have passed between the previous
+			//frame and the current one.
+			void GenerateDeltaTime();
 
-		//Iterate through all the gameobjects and render them.
-		void Render();
+			//Process the user's input and return true if
+			//the input determines that the scene loop should
+			//continue (and return false if it shouldn't).
+			bool GetInput();
 
-		//A vague method intended to be used to update the state
-		//of game objects and return true if the scene loop should
-		//continue (and return false if it shouldn't).
-		virtual bool Update() = 0;
+			//Process a key down event and return true if
+			//the input determines that the scene loop should
+			//continue (and return false if it shouldn't).
+			virtual bool KeyDown(SDL_Keycode keycode) = 0;
 
-		//The quantity of seconds that have passed between the previous frame and
-		//the current one.
-		float deltaTime = 0;
+			//Process a mouse event and return true if
+			//the input determines that the scene loop should
+			//continue (and return false if it shouldn't).
+			virtual bool MouseDown(SDL_MouseButtonEvent mousebutton) = 0;
 
-		//A value which is true if the "Initialise" method has been called
-		//(and is false if it hasn't).
-		bool initialised = false;
+			//Iterate through all the gameobjects and render them.
+			void Render();
 
-		//The list of gameobjects to be rendered.
-		//vector<GameObject*> gameObjects;
-		map<int, GameObject*> gameObjects;
+			//A vague method intended to be used to update the state
+			//of game objects and return true if the scene loop should
+			//continue (and return false if it shouldn't).
+			virtual bool Update() = 0;
 
-		//The name of the next scene to be loaded.
-		string nextScene = "";
+			//The quantity of seconds that have passed between the previous frame and
+			//the current one.
+			float deltaTime = 0;
 
-		//The time stamp of the previous frame.
-		float previousTimeStamp = 0;
+			//A value which is true if the "Initialise" method has been called
+			//(and is false if it hasn't).
+			bool initialised = false;
 
-		//A pointer to the resource manager.
-		ResourceManager* resourceManager;
+			//The list of gameobjects to be rendered.
+			//vector<GameObject*> gameObjects;
+			map<int, GameObject*> gameObjects;
 
-		//The sdl window manager of the current openGL
-		//context (used to clear the display and swap buffers).
-		SDLWindowManager* sdlWindowManager;
+			//The name of the next scene to be loaded.
+			string nextScene = "";
 
-		SkyBox* skybox;
+			//The time stamp of the previous frame.
+			float previousTimeStamp = 0;
+
+			//A pointer to the resource manager.
+			ResourceManager* resourceManager;
+
+			//The sdl window manager of the current openGL
+			//context (used to clear the display and swap buffers).
+			SDLWindowManager* sdlWindowManager;
+
+			SkyBox* skybox;
 	};
 
 }
