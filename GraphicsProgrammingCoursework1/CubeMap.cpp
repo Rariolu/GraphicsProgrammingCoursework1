a243be9f7e2169d8e5dcc92ad786d8236bcd86de
Rariolu <rcoyneyr82011@gmail.com>
2020-05-05 19:38:27 +0100

    New namespace

--- GraphicsProgrammingCoursework1/CubeMap.cpp
+++ GraphicsProgrammingCoursework1/CubeMap.cpp
@@ -1,55 +1,60 @@
 #include "CubeMap.h"
 
-CubeMap::CubeMap(string facePath,string extension) :
-	CubeMap(GetFacePaths(facePath,extension))
+namespace GraphicsProgramming
 {
 
-}
-
-CubeMap::CubeMap(string facePaths[faceNum])
-{
-	glGenTextures(1, &textureHandler);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, textureHandler);
-	int width;
-	int height;
-	int nrChannels;
-	for (unsigned int i = 0; i < faceNum; i++)
+	CubeMap::CubeMap(string facePath, string extension) :
+		CubeMap(GetFacePaths(facePath, extension))
 	{
-		unsigned char* data = stbi_load(facePaths[i].c_str(), &width, &height, &nrChannels, 0);
-		if (data)
-		{
-			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		}
-		else
-		{
-			Log("Cubemap texture failed to load at path: " + facePaths[i]);
-		}
-		stbi_image_free(data);
 
 	}
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
-}
-
-void CubeMap::Bind()
-{
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, textureHandler);
-}
-
-string* CubeMap::GetFacePaths(string facePath, string extension)
-{
-	return new string[6]
+	CubeMap::CubeMap(string facePaths[faceNum])
 	{
-		facePath+"_posX"+extension,
-		facePath+"_negX"+extension,
-		facePath+"_posY"+extension,
-		facePath+"_negY"+extension,
-		facePath+"_posZ"+extension,
-		facePath+"_negZ"+extension
-	};
+		glGenTextures(1, &textureHandler);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, textureHandler);
+		int width;
+		int height;
+		int nrChannels;
+		for (unsigned int i = 0; i < faceNum; i++)
+		{
+			unsigned char* data = stbi_load(facePaths[i].c_str(), &width, &height, &nrChannels, 0);
+			if (data)
+			{
+				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			}
+			else
+			{
+				Log("Cubemap texture failed to load at path: " + facePaths[i]);
+			}
+			stbi_image_free(data);
+
+		}
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	}
+
+	void CubeMap::Bind()
+	{
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, textureHandler);
+	}
+
+	string* CubeMap::GetFacePaths(string facePath, string extension)
+	{
+		return new string[6]
+		{
+			facePath + "_posX" + extension,
+			facePath + "_negX" + extension,
+			facePath + "_posY" + extension,
+			facePath + "_negY" + extension,
+			facePath + "_posZ" + extension,
+			facePath + "_negZ" + extension
+		};
+	}
+
 }
--- GraphicsProgrammingCoursework1/CubeMap.h
+++ GraphicsProgrammingCoursework1/CubeMap.h
@@ -6,18 +6,23 @@
 #include "stb_image.h"
 #include "Logger.h"
 
-class CubeMap
+namespace GraphicsProgramming
 {
-	private:
-		const static unsigned int faceNum = 6;
-	public:
-		CubeMap(string facePath,string extension);
-		CubeMap(string facePaths[faceNum]);
-		void Bind();
-	private:
-		string* GetFacePaths(string facePath, string extension);
-		GLuint textureHandler;
-		
-};
+
+	class CubeMap
+	{
+		private:
+			const static unsigned int faceNum = 6;
+		public:
+			CubeMap(string facePath, string extension);
+			CubeMap(string facePaths[faceNum]);
+			void Bind();
+		private:
+			string* GetFacePaths(string facePath, string extension);
+			GLuint textureHandler;
+
+	};
+
+}
 
 #endif
