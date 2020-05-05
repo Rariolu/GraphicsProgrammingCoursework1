cfca81ff3224d0497634017d862185848b1c6dfa
Rariolu <rcoyneyr82011@gmail.com>
2020-05-05 19:44:36 +0100

    Changed "LoadText" method to return string pointer rather than string value.

--- GraphicsProgrammingCoursework1/Util.cpp
+++ GraphicsProgrammingCoursework1/Util.cpp
@@ -1,79 +1,84 @@
 #include "Util.h"
 
-unsigned int FindIndexOf(unsigned int start, const char* str, unsigned int length, char token)
+namespace GraphicsProgramming
 {
-	unsigned int result = start;
-	while (result < length)
+
+	unsigned int FindIndexOf(unsigned int start, const char* str, unsigned int length, char token)
 	{
-		result++;
-		if (str[result] == token)
+		unsigned int result = start;
+		while (result < length)
 		{
-			break;
-		}
-	}
-
-	return result;
-}
-
-string LoadText(string filename)
-{
-	InputFileStream file;
-	file.open(filename.c_str());
-
-	string output;
-	string currentLine;
-	if (file.is_open())
-	{
-		while (file.good())
-		{
-			std::getline(file, currentLine);
-			output += currentLine + "\n";
-		}
-	}
-	return output;
-}
-
-int RandomNumber(int min, int max)
-{
-	return min + (rand() % (max - min + 1));
-}
-
-Matrix RemoveTranslation(Matrix mat)
-{
-	Mat3 m3 = Mat3(mat);
-	return Matrix(m3);
-	//TODO: Replace with more efficient method
-}
-
-vector<string> SplitString(string str, char delimiter)
-{
-	vector<string> elems;
-
-	const char* cstr = str.c_str();
-	unsigned int strLength = str.length();
-	unsigned int start = 0;
-	unsigned int end = 0;
-
-	while (end <= strLength)
-	{
-		while (end <= strLength)
-		{
-			if (cstr[end] == delimiter)
+			result++;
+			if (str[result] == token)
 			{
 				break;
 			}
-			end++;
 		}
 
-		elems.push_back(str.substr(start, end - start));
-		start = end + 1;
-		end = start;
+		return result;
 	}
 
-	return elems;
-}
+	string* LoadText(string filename)
+	{
+		InputFileStream file;
+		file.open(filename.c_str());
 
-float SquareValue(float value)
-{
-	return value * value;
+		string output;
+		string currentLine;
+		if (file.is_open())
+		{
+			while (file.good())
+			{
+				std::getline(file, currentLine);
+				output += currentLine + "\n";
+			}
+			return new string(output);
+		}
+		return nullptr;
+	}
+
+	int RandomNumber(int min, int max)
+	{
+		return min + (rand() % (max - min + 1));
+	}
+
+	Matrix RemoveTranslation(Matrix mat)
+	{
+		Mat3 m3 = Mat3(mat);
+		return Matrix(m3);
+		//TODO: Replace with more efficient method
+	}
+
+	vector<string> SplitString(string str, char delimiter)
+	{
+		vector<string> elems;
+
+		const char* cstr = str.c_str();
+		unsigned int strLength = str.length();
+		unsigned int start = 0;
+		unsigned int end = 0;
+
+		while (end <= strLength)
+		{
+			while (end <= strLength)
+			{
+				if (cstr[end] == delimiter)
+				{
+					break;
+				}
+				end++;
+			}
+
+			elems.push_back(str.substr(start, end - start));
+			start = end + 1;
+			end = start;
+		}
+
+		return elems;
+	}
+
+	float SquareValue(float value)
+	{
+		return value * value;
+	}
 }
--- GraphicsProgrammingCoursework1/Util.h
+++ GraphicsProgrammingCoursework1/Util.h
@@ -3,37 +3,55 @@
 
 #include "Aliases.h"
 
-//Find the index of a requested character after a given start point in a string.
-unsigned int FindIndexOf(unsigned int start, const char* str, unsigned int length, char token);
-
-//Return the text stored in the given file path.
-string LoadText(string filename);
-
-//Takes two values, returns the largest of them.
-template<typename T>
-T max(T a, T b)
+namespace GraphicsProgramming
 {
-	return a > b ? a : b;
+
+	//Find the index of a requested character after a given start point in a string.
+	unsigned int FindIndexOf(unsigned int start, const char* str, unsigned int length, char token);
+
+	//Return the text stored in the given file path.
+	string* LoadText(string filename);
+
+	//Takes two values, returns the largest of them.
+	template<typename T>
+	T max(T a, T b)
+	{
+		return a > b ? a : b;
+	}
+
+	//Takes two values, returns the smallest of them.
+	template<typename T>
+	T min(T a, T b)
+	{
+		return a < b ? a : b;
+	}
+
+	//Returns a random number within the given range.
+	int RandomNumber(int min, int max);
+
+	//Remove translation units from the given matrix.
+	Matrix RemoveTranslation(Matrix mat);
+
+	//Get a pointer of type T* from a given
+	//U*.
+	template<class T, class U>
+	T* GetPointerOfType(U* ptr)
+	{
+		return dynamic_cast<T*>(ptr);
+		//if (typeid(*ptr) == typeid(T))
+		//{
+		//	return ((T*)ptr);
+		//}
+		//return nullptr;
+	}
+
+	//Split the given string into multiple
+	//substrings using a given character.
+	vector<string> SplitString(string str, char delimiter);
+
+	//Multiply the given value by itself and return the result.
+	float SquareValue(float value);
+
 }
-
-//Takes two values, returns the smallest of them.
-template<typename T>
-T min(T a, T b)
-{
-	return a < b ? a : b;
-}
-
-//Returns a random number within the given range.
-int RandomNumber(int min, int max);
-
-//Remove translation units from the given matrix.
-Matrix RemoveTranslation(Matrix mat);
-
-//Split the given string into multiple
-//substrings using a given character.
-vector<string> SplitString(string str, char delimiter);
-
-//Multiply the given value by itself and return the result.
-float SquareValue(float value);
 
 #endif
