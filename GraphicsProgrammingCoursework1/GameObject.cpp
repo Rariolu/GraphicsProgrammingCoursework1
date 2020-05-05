a22467cdc7c76e3a6cb96c6b4c1b722096d78e94
Rariolu <rcoyneyr82011@gmail.com>
2020-05-06 00:04:45 +0100

    Refactored initialisation

--- GraphicsProgrammingCoursework1/GameObject.cpp
+++ GraphicsProgrammingCoursework1/GameObject.cpp
@@ -1,94 +1,110 @@
 #include "GameObject.h"
 
-ResourceManager* GameObject::rescManager = ResourceManager::Instance();
-
-GameObject::GameObject(string meshName, string shaderName, string textureName) : GameObject(rescManager->GetMesh(meshName),(GameObjectShader*)rescManager->GetShader(shaderName), rescManager->GetModelTexture(textureName))
+namespace GraphicsProgramming
 {
 
-}
+	ResourceManager* GameObject::rescManager = ResourceManager::Instance();
 
-GameObject::GameObject(Mesh* mesh, GameObjectShader* shader, ModelTexture* texture)
-{
-	camera = nullptr;
-	collider = nullptr;
-	this->mesh = mesh;
-	this->shader = shader;
-	this->texture = texture;
-}
-
-GameObject::~GameObject()
-{
-
-}
-
-bool GameObject::CollidesWith(GameObject* other)
-{
-	//Return false if the other gameobject or this gameobject's
-	//collider is null.
-	if (!other || !collider)
+	GameObject::GameObject(string meshName, string shaderName, string textureName)
+	//	: GameObject(rescManager->GetMesh(meshName), (GameObjectShader*)rescManager->GetShader(shaderName), rescManager->GetModelTexture(textureName))
 	{
-		return false;
+		Mesh* mesh = rescManager->GetMesh(meshName);
+		Shader* shader = rescManager->GetShader(shaderName);
+		ModelTexture* texture = rescManager->GetModelTexture(textureName);
+		GameObjectShader* gameObjectShader = gameObjectShader = dynamic_cast<GameObjectShader*>(shader);
+		
+		Init(mesh, gameObjectShader, texture);
 	}
 
-	//Return false if this gameobject or the other gameobject
-	//is inactive.
-	if (!(IsActive() && other->IsActive()))
+	GameObject::GameObject(Mesh* mesh, GameObjectShader* shader, ModelTexture* texture)
 	{
-		return false;
+		Init(mesh, shader, texture);
 	}
 
-	//Return false if the other gameobject's collider is null.
-	Collider* otherCollider = other->GetCollider();
-	if (!otherCollider)
+	GameObject::~GameObject()
 	{
-		return false;
+
 	}
 
-	//Check for collisions between the two colliders.
-	return collider->CollidesWith(otherCollider) || otherCollider->CollidesWith(collider);
-}
-
-Collider* GameObject::GetCollider()
-{
-	return collider;
-}
-
-bool GameObject::IsActive()
-{
-	return isActive;
-}
-
-void GameObject::Render()
-{
-	if (isActive && camera)
+	bool GameObject::CollidesWith(GameObject* other)
 	{
-		//Bind the shader
-		shader->Bind();
+		//Return false if the other gameobject or this gameobject's
+		//collider is null.
+		if (!other || !collider)
+		{
+			return false;
+		}
 
-		//Update the shader's uniform variables
-		//according to the position, orientation, and scaling
-		//of this object as well as the camera.
-		shader->Update(this, camera);
+		//Return false if this gameobject or the other gameobject
+		//is inactive.
+		if (!(IsActive() && other->IsActive()))
+		{
+			return false;
+		}
 
-		//Bind the texture
-		texture->Bind();
+		//Return false if the other gameobject's collider is null.
+		Collider* otherCollider = other->GetCollider();
+		if (!otherCollider)
+		{
+			return false;
+		}
 
-		//Render the mesh
-		mesh->Render();
+		//Check for collisions between the two colliders.
+		return collider->CollidesWith(otherCollider) || otherCollider->CollidesWith(collider);
 	}
-}
 
-void GameObject::SetActive(bool active)
-{
-	isActive = active;
-}
+	Collider* GameObject::GetCollider()
+	{
+		return collider;
+	}
 
-void GameObject::SetCamera(Camera* cam)
-{
-	camera = cam;
-}
+	bool GameObject::IsActive()
+	{
+		return isActive;
+	}
 
-void GameObject::SetCollider(Collider* col)
-{
-	collider = col;
+	void GameObject::Render()
+	{
+		if (isActive && camera)
+		{
+			//Bind the shader
+			shader->Bind();
+
+			//Update the shader's uniform variables
+			//according to the position, orientation, and scaling
+			//of this object as well as the camera.
+			shader->Update(this, camera);
+
+			//Bind the texture
+			texture->Bind();
+
+			//Render the mesh
+			mesh->Render();
+		}
+	}
+
+	void GameObject::SetActive(bool active)
+	{
+		isActive = active;
+	}
+
+	void GameObject::SetCamera(Camera* cam)
+	{
+		camera = cam;
+	}
+
+	void GameObject::SetCollider(Collider* col)
+	{
+		collider = col;
+	}
+
+	void GameObject::Init(Mesh* mesh, GameObjectShader* shader, ModelTexture* texture)
+	{
+		camera = nullptr;
+		collider = nullptr;
+		this->mesh = mesh;
+		this->shader = shader;
+		this->texture = texture;
+	}
+
 }
--- GraphicsProgrammingCoursework1/GameObject.h
+++ GraphicsProgrammingCoursework1/GameObject.h
@@ -5,66 +5,75 @@
 #include "ResourceManager.h"
 #include "Collider.h"
 
-class GameObject : public Transform
+namespace GraphicsProgramming
 {
-public:
-	GameObject(string meshName, string shaderName, string textureName);
-	GameObject(Mesh* mesh, GameObjectShader* shader, ModelTexture* texture);
-	~GameObject();
 
-	//Detects a collision between two GameObjects
-	//using their colliders.
-	bool CollidesWith(GameObject* other);
+	class GameObject : public Transform
+	{
+	public:
+		GameObject(string meshName, string shaderName, string textureName);
+		GameObject(Mesh* mesh, GameObjectShader* shader, ModelTexture* texture);
+		~GameObject();
 
-	//Get the collider that's attached to this gameobject.
-	Collider* GetCollider();
+		//Detects a collision between two GameObjects
+		//using their colliders.
+		bool CollidesWith(GameObject* other);
 
-	//Returns true if this GameObject is "active"
-	//(meaning it is visible and can be collided with).
-	bool IsActive();
+		//Get the collider that's attached to this gameobject.
+		Collider* GetCollider();
 
-	//Bind the relevant mesh, shader, and texture
-	//in order to render this model to the screen.
-	void Render();
+		//Returns true if this GameObject is "active"
+		//(meaning it is visible and can be collided with).
+		bool IsActive();
 
-	//Set the active state of this gameobject
-	//(when it's active, it is rendered and
-	//can be collided with).
-	void SetActive(bool active);
+		//Bind the relevant mesh, shader, and texture
+		//in order to render this model to the screen.
+		void Render();
 
-	//Set the camera that's used to render this gameobject.
-	void SetCamera(Camera* cam);
+		//Set the active state of this gameobject
+		//(when it's active, it is rendered and
+		//can be collided with).
+		void SetActive(bool active);
 
-protected:
-	//Set the collider that's used to determine
-	//collisions with other gameobjects.
-	void SetCollider(Collider* col);
+		//Set the camera that's used to render this gameobject.
+		void SetCamera(Camera* cam);
 
-private:
-	//The camera that's used to render this gameobject.
-	Camera* camera;
+	protected:
+		//Set the collider that's used to determine
+		//collisions with other gameobjects.
+		void SetCollider(Collider* col);
 
-	//The collider that's used to determine
-	//collisions with other gameobjects.
-	Collider* collider;
+		//A pointer to the "ResourceManager" so that
+		//resources can be loaded (e.g. shaders, textures, etc.).
+		static ResourceManager* rescManager;
+	private:
 
-	//The active state of this gameobject.
-	bool isActive = true;
+		void Init(Mesh* mesh, GameObjectShader* shader, ModelTexture* texture);
 
-	//The mesh (collection of vertices) to determine
-	//the gameobject's shape.
-	Mesh* mesh;
+		//The camera that's used to render this gameobject.
+		Camera* camera;
 
-	//A pointer to the "ResourceManager" so that
-	//resources can be loaded (e.g. shaders, textures, etc.).
-	static ResourceManager* rescManager;
+		//The collider that's used to determine
+		//collisions with other gameobjects.
+		Collider* collider;
 
-	//The shader that's used to render the mesh.
-	GameObjectShader* shader;
+		//The active state of this gameobject.
+		bool isActive = true;
 
-	//The texture that's applied to the mesh in order
-	//to specify its appearance.
-	ModelTexture* texture;
-};
+		//The mesh (collection of vertices) to determine
+		//the gameobject's shape.
+		Mesh* mesh;
+
+
+
+		//The shader that's used to render the mesh.
+		GameObjectShader* shader;
+
+		//The texture that's applied to the mesh in order
+		//to specify its appearance.
+		ModelTexture* texture;
+	};
+
+}
 
 #endif
