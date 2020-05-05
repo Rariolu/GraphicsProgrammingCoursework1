2b63cef07b455d3ca34bc8bbe1fe41879b6be18a
Rariolu <rcoyneyr82011@gmail.com>
2020-05-06 00:07:50 +0100

    Creating default shaders when files not loaded

--- GraphicsProgrammingCoursework1/Shader.cpp
+++ GraphicsProgrammingCoursework1/Shader.cpp
@@ -1,116 +1,146 @@
 #include "Shader.h"
 
-Shader::Shader(string geomFile, string vertFile, string fragFile)
-	: Shader(new ShaderDef[3]{{geomFile,GL_GEOMETRY_SHADER},{vertFile,GL_VERTEX_SHADER},{fragFile,GL_FRAGMENT_SHADER}},3)
+namespace GraphicsProgramming
 {
 
-}
-
-Shader::Shader(string vertFile, string fragFile) :
-Shader(new ShaderDef[2]{{vertFile,GL_VERTEX_SHADER},{fragFile,GL_FRAGMENT_SHADER} },2)
-{
-	
-}
-
-Shader::Shader(ShaderDef* shaderDefinitions, unsigned int length)
-{
-	program = glCreateProgram();
-
-	for(unsigned int i = 0; i < length; i++)
+	Shader::Shader(string geomFile, string vertFile, string fragFile)
+		: Shader(new ShaderDef[3]{ {geomFile,GL_GEOMETRY_SHADER},{vertFile,GL_VERTEX_SHADER},{fragFile,GL_FRAGMENT_SHADER} }, 3)
 	{
-		ShaderDef shaderDef = shaderDefinitions[i];
-		string shaderText = LoadText(shaderDef.file);
-		shaders.push_back(CreateShader(shaderText, shaderDef.type));
+
 	}
 
-	AttachShaders();
-
-	glLinkProgram(program);
-	glValidateProgram(program);
-}
-
-void Shader::Bind()
-{
-	glUseProgram(program);
-}
-
-void Shader::SetFloat(GLuint address, float value)
-{
-	glUniform1f(address, value);
-}
-
-void Shader::SetFloat(string name, float value)
-{
-	SetFloat(glGetUniformLocation(program, name.c_str()), value);
-}
-
-void Shader::SetInt(GLuint address, int value)
-{
-	glUniform1i(address, value);
-}
-
-void Shader::SetInt(string name, int value)
-{
-	SetInt(glGetUniformLocation(program, name.c_str()), value);
-}
-
-void Shader::SetMatrix(GLuint address, Matrix value)
-{
-	glUniformMatrix4fv(address, 1, GL_FALSE, &value[0][0]);
-}
-
-void Shader::SetMatrix(string name, Matrix value)
-{
-	SetMatrix(glGetUniformLocation(program, name.c_str()), value);
-}
-
-void Shader::SetVec2(GLuint address, Vec2 value)
-{
-	glUniform2fv(address, 1, &value[0]);
-}
-
-void Shader::SetVec2(string name, Vec2 value)
-{
-	SetVec2(glGetUniformLocation(program, name.c_str()), value);
-}
-
-void Shader::SetVec3(GLuint address, Vec3 value)
-{
-	glUniform3fv(address, 1, &value[0]);
-}
-
-void Shader::SetVec3(string name, Vec3 value)
-{
-	SetVec3(glGetUniformLocation(program, name.c_str()), value);
-}
-
-GLuint Shader::GetProgram()
-{
-	return program;
-}
-
-void Shader::AttachShaders()
-{
-	for (unsigned int i = 0; i < shaders.size(); i++)
+	Shader::Shader(string vertFile, string fragFile) :
+		Shader(new ShaderDef[2]{ {vertFile,GL_VERTEX_SHADER},{fragFile,GL_FRAGMENT_SHADER} }, 2)
 	{
-		glAttachShader(program, shaders[i]);
-	}
-}
 
-GLuint Shader::CreateShader(string text, unsigned int type)
-{
-	GLuint shader = glCreateShader(type);
-
-	if (!shader)
-	{
-		Log("Shader failed to be created.");
 	}
 
-	const GLchar* stringsource[] = { text.c_str() };
-	GLint lengths[] = { text.length() };
+	Shader::Shader(ShaderDef* shaderDefinitions, unsigned int length)
+	{
+		program = glCreateProgram();
 
-	glShaderSource(shader, 1, stringsource, lengths);
-	glCompileShader(shader);
+		for (unsigned int i = 0; i < length; i++)
+		{
+			ShaderDef shaderDef = shaderDefinitions[i];
+			string* shaderText = LoadText(shaderDef.file);
+			string text = "";
+			if (shaderText)
+			{
+				text = *shaderText;
+				delete shaderText;
+			}
+			/*else
+			{
+				Log(shaderDef.file+" failed to load.");
+				map<unsigned int, string>::iterator iter = quadMShaders.find(shaderDef.type);
+				if (iter != quadMShaders.end())
+				{
+					text = iter->second;
+				}
+			}*/
+			shaders.push_back(CreateShader(text, shaderDef.type));
+		}
 
-	return shader;
+		AttachShaders();
+
+		glLinkProgram(program);
+		glValidateProgram(program);
+	}
+
+	Shader::~Shader()
+	{
+		for (unsigned int i = 0; i < shaders.size(); i++)
+		{
+			glDetachShader(program, shaders[i]);
+			glDeleteShader(shaders[i]);
+		}
+		glDeleteProgram(program);
+	}
+
+	void Shader::Bind()
+	{
+		glUseProgram(program);
+	}
+
+	void Shader::SetFloat(GLuint address, float value)
+	{
+		glUniform1f(address, value);
+	}
+
+	void Shader::SetFloat(string name, float value)
+	{
+		SetFloat(glGetUniformLocation(program, name.c_str()), value);
+	}
+
+	void Shader::SetInt(GLuint address, int value)
+	{
+		glUniform1i(address, value);
+	}
+
+	void Shader::SetInt(string name, int value)
+	{
+		SetInt(glGetUniformLocation(program, name.c_str()), value);
+	}
+
+	void Shader::SetMatrix(GLuint address, Matrix value)
+	{
+		glUniformMatrix4fv(address, 1, GL_FALSE, &value[0][0]);
+	}
+
+	void Shader::SetMatrix(string name, Matrix value)
+	{
+		SetMatrix(glGetUniformLocation(program, name.c_str()), value);
+	}
+
+	void Shader::SetVec2(GLuint address, Vec2 value)
+	{
+		glUniform2fv(address, 1, &value[0]);
+	}
+
+	void Shader::SetVec2(string name, Vec2 value)
+	{
+		SetVec2(glGetUniformLocation(program, name.c_str()), value);
+	}
+
+	void Shader::SetVec3(GLuint address, Vec3 value)
+	{
+		glUniform3fv(address, 1, &value[0]);
+	}
+
+	void Shader::SetVec3(string name, Vec3 value)
+	{
+		SetVec3(glGetUniformLocation(program, name.c_str()), value);
+	}
+
+	GLuint Shader::GetProgram()
+	{
+		return program;
+	}
+
+	void Shader::AttachShaders()
+	{
+		for (unsigned int i = 0; i < shaders.size(); i++)
+		{
+			glAttachShader(program, shaders[i]);
+		}
+	}
+
+	GLuint Shader::CreateShader(string text, unsigned int type)
+	{
+		GLuint shader = glCreateShader(type);
+
+		if (!shader)
+		{
+			Log("Shader failed to be created.");
+		}
+
+		const GLchar* stringsource[] = { text.c_str() };
+		GLint lengths[] = { text.length() };
+
+		glShaderSource(shader, 1, stringsource, lengths);
+		glCompileShader(shader);
+
+		return shader;
+	}
+
 }
--- GraphicsProgrammingCoursework1/Shader.h
+++ GraphicsProgrammingCoursework1/Shader.h
@@ -4,65 +4,73 @@
 #include "glew.h"
 #include "Util.h"
 #include "Logger.h"
+#include "Recs.h"
 
-struct ShaderDef
+namespace GraphicsProgramming
 {
-	string file;
-	unsigned int type;
-};
 
-class Shader
-{
-	public:
-		Shader(string geomFile, string vertFile, string fragFile);
-		Shader(string vertFile, string fragFile);
-		Shader(ShaderDef* shaderDefinitions, unsigned int length);
+	struct ShaderDef
+	{
+		string file;
+		unsigned int type;
+	};
 
-		//Tell openGL to use the executable program associated with this
-		//"AbstractShader".
-		void Bind();
+	class Shader
+	{
+		public:
+			Shader(string geomFile, string vertFile, string fragFile);
+			Shader(string vertFile, string fragFile);
+			Shader(ShaderDef* shaderDefinitions, unsigned int length);
 
-		//Set the value of the float uniform
-		//at the given address.
-		void SetFloat(GLuint address, float value);
-		void SetFloat(string name, float value);
+			virtual ~Shader();
 
-		void SetInt(GLuint address, int value);
-		void SetInt(string name, int value);
+			//Tell openGL to use the executable program associated with this
+			//"AbstractShader".
+			void Bind();
 
-		//Set the value of the mat4 uniform
-		//at the given address.
-		void SetMatrix(GLuint address, Matrix value);
-		void SetMatrix(string name, Matrix value);
+			//Set the value of the float uniform
+			//at the given address.
+			void SetFloat(GLuint address, float value);
+			void SetFloat(string name, float value);
 
-		//Set the value of the vec2 uniform
-		//at the given address.
-		void SetVec2(GLuint address, Vec2 value);
-		void SetVec2(string name, Vec2 value);
+			void SetInt(GLuint address, int value);
+			void SetInt(string name, int value);
 
-		//Set the value of the vec3 uniform
-		//at the given address.
-		void SetVec3(GLuint address, Vec3 value);
-		void SetVec3(string name, Vec3 value);
+			//Set the value of the mat4 uniform
+			//at the given address.
+			void SetMatrix(GLuint address, Matrix value);
+			void SetMatrix(string name, Matrix value);
 
-	protected:
-		//Return the address of the executable program.
-		GLuint GetProgram();
-	private:
+			//Set the value of the vec2 uniform
+			//at the given address.
+			void SetVec2(GLuint address, Vec2 value);
+			void SetVec2(string name, Vec2 value);
 
-		//Attach the openGL shaders to the program
-		//on the GPU.
-		void AttachShaders();
+			//Set the value of the vec3 uniform
+			//at the given address.
+			void SetVec3(GLuint address, Vec3 value);
+			void SetVec3(string name, Vec3 value);
 
-		//Create an openGL shader from the given text and type
-		//before returning its address.
-		GLuint CreateShader(string text, unsigned int type);
+		protected:
+			//Return the address of the executable program.
+			GLuint GetProgram();
+		private:
 
-		//The address of the executable program.
-		GLuint program;
+			//Attach the openGL shaders to the program
+			//on the GPU.
+			void AttachShaders();
 
-		//The addresses of GLSL shaders
-		vector<GLuint> shaders;
-};
+			//Create an openGL shader from the given text and type
+			//before returning its address.
+			GLuint CreateShader(string text, unsigned int type);
+
+			//The address of the executable program.
+			GLuint program;
+
+			//The addresses of GLSL shaders
+			vector<GLuint> shaders;
+	};
+
+}
 
 #endif
