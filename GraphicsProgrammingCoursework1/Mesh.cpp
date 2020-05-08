7abd2776b7b3e3aa3305c925feaed1ab8e911aa4
Rariolu <rcoyneyr82011@gmail.com>
2020-05-08 06:34:52 +0100

    Misc

--- GraphicsProgrammingCoursework1/Mesh.cpp
+++ GraphicsProgrammingCoursework1/Mesh.cpp
@@ -56,45 +56,6 @@ namespace GraphicsProgramming
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, model->indices.size() * sizeof(model->indices[0]), &model->indices[0], GL_STATIC_DRAW); //move the data to the GPU - type of data, size of data, starting address (pointer) of data, where do we store the data on the GPU
 
 		glBindVertexArray(0); // unbind our VAO
-		//drawCount = model->indices.size();
-
-		////Generate and bind vertex array.
-		//glGenVertexArrays(1, &vertexArrayObject);
-		//glBindVertexArray(vertexArrayObject);
-
-		//unsigned int positionsSize = model->positions.size() * sizeof(model->positions[0]);
-		////unsigned int uvSize = model->uvCoords.size() * sizeof(model->uvCoords[0]);
-		//unsigned int uvSize = model->texCoords.size() * sizeof(model->texCoords[0]);
-		//unsigned int normalSize = model->normals.size() * sizeof(model->normals[0]);
-		//unsigned int indexSize = model->indices.size() * sizeof(model->indices[0]);
-
-		////Generate buffers
-		//glGenBuffers(bufferCount, vertexArrayBuffers);
-
-		////Bind and populate Position buffer
-		//glBindBuffer(GL_ARRAY_BUFFER, vertexArrayBuffers[0]);
-		//glBufferData(GL_ARRAY_BUFFER, positionsSize, &model->positions[0], GL_STATIC_DRAW);
-		//glEnableVertexAttribArray(0);
-		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-		////Bind and populate UV buffer
-		//glBindBuffer(GL_ARRAY_BUFFER, vertexArrayBuffers[1]);
-		////glBufferData(GL_ARRAY_BUFFER, uvSize, &model->uvCoordinates[1], GL_STATIC_DRAW);
-		//glBufferData(GL_ARRAY_BUFFER, uvSize, &model->texCoords[1], GL_STATIC_DRAW);
-		//glEnableVertexAttribArray(1);
-		//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-		////Bind and populate Normal buffer
-		//glBindBuffer(GL_ARRAY_BUFFER, vertexArrayBuffers[2]);
-		//glBufferData(GL_ARRAY_BUFFER, normalSize, &model->normals[0], GL_STATIC_DRAW);
-		//glEnableVertexAttribArray(2);
-		//glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-		////Bind and populate Index buffer
-		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertexArrayBuffers[3]);
-		//glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexSize, &model->indices[0], GL_STATIC_DRAW);
-
-		//glBindVertexArray(0);
 	}
 
 }
